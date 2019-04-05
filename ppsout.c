3ce0552492bbe06d2d3dde8844b06e2903303c7e
electronicslover <luizpaulodamaceno@live.com>
2019-04-05 17:11:27 -0300

    Especifique quanto tempo a variável demora para estourar e faça as rodadas por nS de acordo com o tempo de execução.

--- ppsout.c
+++ ppsout.c
@@ -1,21 +1,22 @@
 #include <pigpio.h>
 //usando isso podemos fazer com que o agendador de io pule para outros processos e depois volte a esse para realizar as operacoes
 //compile usando: gcc -o ppsout ppsout.c -lrt -lpthread -lpigpio
+long int inicial;
+long int diferenca;
+int runs = 100;
+struct timespec agora;
+struct timespec depois;
+
 void micro_delay (int delay_us)
 {
-        //vars:
-        long int inicial;
-        long int diferenca;
-        struct timespec agora;
-
         clock_gettime(CLOCK_REALTIME, &agora);  //primeira chamada da funcao 
         inicial = agora.tv_nsec;        //valor em ns para normalizacao
         for (; ;)
         {
-                clock_gettime(CLOCK_REALTIME, &agora);  //pega um timestamp todo inicio do loop
-                diferenca = agora.tv_nsec - inicial;    //delta t
-                if (diferenca < 0)
-                        diferenca += 1000000000;        //(reinicia a cada 1 segundo)
+                clock_gettime(CLOCK_REALTIME, &depois);  //pega um timestamp todo inicio do loop
+                diferenca = ((depois.tv_nsec - inicial) / (runs));    //delta t
+                if ((depois.tv_nsec - inicial) < 0)
+                        diferenca += 1000000000;        //(reinicia a cada 1 segundo, registrador estoura a cada 1 segundo)
                 if (diferenca > (delay_us * 1000))              //atraso em ns
                         break;
         }
@@ -28,8 +29,8 @@ void main()
         for (; ;)
         {
                 gpioWrite(4, 1);        //pino ligado
-                micro_delay(200);       //chamada da funcao com tOn desejado
+                micro_delay(100);       //chamada da funcao com tOn desejado
                 gpioWrite(4, 0);        //pino desligado
-                micro_delay(999800);    //chamada da funcao com tOff desejado
+                micro_delay(499900);    //chamada da funcao com tOff desejado
         }
 }
