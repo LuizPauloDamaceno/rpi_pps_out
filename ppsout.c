20547950b62010ab4398ee53bf10bab6cb639ada
electronicslover <luizpaulodamaceno@live.com>
2019-04-25 10:22:31 -0300

    adicionando travas e mudando opções de compilação

--- ppsout.c
+++ ppsout.c
@@ -1,13 +1,17 @@
+#include <pthread.h>
 #include <pigpio.h>
+#include <stdio.h>
 //usando isso podemos fazer com que o agendador de io pule para outros processos e depois volte a esse para realizar as operacoes
-//compile usando: gcc -o ppsout ppsout.c -lrt -lpthread -lpigpio
-long int inicial;
-long int diferenca;
-int runs = 100;
+//compile usando: gcc -o ppsout ppsout.c -lrt -lpthread -lpigpio -lm -fno-pic -march=armv7-a -mfpu=neon-vfpv4 -mhard-float -mfloat-abi=hard -fno-delete-null-pointer-checks -ffast-math -fgcse-lm -fgcse-sm -fsched-spec-load -fforce-addr -fsingle-precision-constant -O3
+long double inicial;
+long double diferenca;
+int runs = 100; //100
 struct timespec agora;
 struct timespec depois;
 
-void micro_delay (int delay_us)
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int micro_delay (int delay_us)
 {
         clock_gettime(CLOCK_REALTIME, &agora);  //primeira chamada da funcao 
         inicial = agora.tv_nsec;        //valor em ns para normalizacao
@@ -22,15 +26,17 @@ void micro_delay (int delay_us)
         }
 }
 
-void main()
+int main()
 {
         gpioInitialise(); //inicializa GPIOs
         gpioSetMode(4, PI_OUTPUT); //seta GPIO4 como saída
         for (; ;)
         {
+                pthread_mutex_lock(&mutex);
                 gpioWrite(4, 1);        //pino ligado
-                micro_delay(100);       //chamada da funcao com tOn desejado
+                micro_delay(200);       //chamada da funcao com tOn desejado
                 gpioWrite(4, 0);        //pino desligado
                 micro_delay(499900);    //chamada da funcao com tOff desejado
+                pthread_mutex_unlock(&mutex);
         }
 }
